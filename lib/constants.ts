d24aa9dec2ebcb315460265444d7826f14e0f352
Daniel Lee <daniellee6925@gmail.com>
2026-02-27 14:43:08 -0800

    fix

---
+++ lib/constants.ts
@@ -0,0 +1,18 @@
+export const SITES = {
+  fitness: {
+    name: "Bill Timlen",
+    alternateName: "William Timlen",
+    jobTitle: "Certified Personal Trainer",
+    businessType: "HealthAndBeautyBusiness",
+    businessName: "Timlen Fitness",
+    domain: "timlenfitness.com",
+    phone: "(201) 555-0476",
+    email: "bill@timlenfitness.com",
+    address: {
+      streetAddress: "310 Washington Street, Unit 1",
+      addressLocality: "Hoboken",
+      addressRegion: "NJ",
+      postalCode: "07030",
+    },
+  },
+};
